5e28c4e6c3bfff0ef618bc05ee589a3b55cd3898
deepikajovy <deepajovy@gmail.com>
2015-06-25 11:19:40 +1200

    Master

---
+++ Meal Planner/Meal Planner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Meal_Planner.Startup))]
+namespace Meal_Planner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
