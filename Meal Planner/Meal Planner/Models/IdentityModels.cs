bcfbe50174eab3de2b13d5ac3df93923decd801f
user <user>
2015-06-25 13:10:05 +1200

    Database Created

--- Meal Planner/Meal Planner/Models/IdentityModels.cs
+++ Meal Planner/Meal Planner/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,8 @@ namespace Meal_Planner.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<MealModel> Meal { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -29,5 +32,9 @@ namespace Meal_Planner.Models
         {
             return new ApplicationDbContext();
         }
+
+        public virtual DbSet<MealModel> Meals { get; set; }
+        public virtual DbSet<IngredientModel> Ingredients { get; set; }
+
     }
 }
