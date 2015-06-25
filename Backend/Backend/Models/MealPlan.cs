c57ab09cb7bc1675ddc7bdb7dfecf60be5bc43e7
user <user@users.noreply.github.com>
2015-06-25 15:22:30 +1200

    add ingrediant, meal and mealplan models

---
+++ Backend/Backend/Models/MealPlan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class MealPlan
+    {
+        public int Id { get; set; }
+        public ApplicationUser User { get; set; }
+        public string Name { get; set; }
+        public List<Meal> Meals { get; set; }
+    }
+}
