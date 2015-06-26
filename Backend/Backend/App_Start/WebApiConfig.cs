4bbab2575e68470454a38edc06e8654486b4a2b8
user <user>
2015-06-26 14:50:31 +1200

    CorsFixed

--- Backend/Backend/App_Start/WebApiConfig.cs
+++ Backend/Backend/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-//using System.Web.Http.Cors;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -16,8 +16,8 @@ namespace Backend
         public static void Register(HttpConfiguration config)
         {
 
-          //var cors = new EnableCorsAttribute("*", "*", "*");
-           // config.EnableCors(cors);
+          var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
