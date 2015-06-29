4f24c57eed1c4ead59166033aa4617b5824a057e
user <user>
2015-06-30 11:59:39 +1200

    added_security

--- Backend/Backend/Models/MealPlan.cs
+++ Backend/Backend/Models/MealPlan.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace Backend.Models
 {
     public class MealPlan
     {
         public int Id { get; set; }
+        [JsonIgnore]
         public ApplicationUser User { get; set; }
         public string Name { get; set; }
         public virtual ICollection<MealPlanItem> MealPlanItems { get; set; }
