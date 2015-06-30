8bfefcae61b4bf986e424e3cef0ea8ad56192eaa
user <user>
2015-06-30 14:02:37 +1200

    added migration runner

---
+++ Backend/Backend/App_Start/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Backend.Models;
+using Backend.Migrations;
+
+
+namespace Backend.App_Start
+{
+    public static class DbConfig
+    {
+        public static void RunDbMigrations()
+        {     
+            Database.SetInitializer<ApplicationDbContext>(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.Database.Initialize(false);
+            }
+        }
+    }
+}
